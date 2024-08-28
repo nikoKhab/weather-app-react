8d91da25a63f8ad427f0c7de0960ab0835ed0ce8
nikoKhab <nmangori.1@gmail.com>
2024-08-28 21:59:33 +0400

    final commit

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -2,16 +2,44 @@ import './App.css';
 import { useRef, useState } from 'react';
 
 function App() {
-  const searchRef = useRef("");
-  const [weathers, setWeatherss] = useState([]);
+  const searchRef = useRef(null);
+  const [weather, setWeather] = useState(null);
+  const apiKey = "ee0e408081f8c0431353de0910743cfa";
 
   function getWeather() {
-    const URL = `https://freetestapi.com/api/v1/weathers?search=${searchRef.current.value}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchRef.current.value}&appid=${apiKey}`;
     fetch(URL)
-      .then(res => res.json())
-      .then(data => setWeatherss(data))
-      .catch(err => console.log(err));
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("City not found");
+        }
+        return res.json();
+      })
+      .then(data => setWeather(data))
+      .catch(err => alert("Please enter a valid city"));
+      
     searchRef.current.value = "";
+  }
+
+  function getWeatherEmoji(weatherId) {
+    switch(true) {
+      case (weatherId >= 200 && weatherId < 300):
+        return "⛈";
+      case (weatherId >= 300 && weatherId < 400):
+        return "🌧";
+      case (weatherId >= 500 && weatherId < 600):
+        return "🌧";
+      case (weatherId >= 600 && weatherId < 700):
+        return "❄";
+      case (weatherId >= 700 && weatherId < 800):
+        return "🌫";
+      case (weatherId === 800):
+        return "☀";
+      case (weatherId >= 801 && weatherId < 810):
+        return "☁";
+      default:
+        return "❓";
+    }
   }
 
   return (
@@ -30,25 +58,26 @@ function App() {
           Search
         </button>
       </div>
-    
-        {weathers.length > 0 ? (
-          <div id="displayData" className='flex align-center justify-center mt-7 bg-orange-500 w-32'>
-            <div className='flex h-[100px] w-[100%] items-center justify-center'>
-              {weathers.map((weather) => (
-                <div key={weather.id}>
-                  <h2>{weather.country}, {weather.city}</h2>
-                  <h2></h2>
-                  <p>Temperature: {weather.temperature}</p>
-                  <p>Description: {weather.weather_description}</p>
-                  <p>Wind speed: {weather.wind_speed}</p>
+      <div className='flex align-center justify-center'>
+        {weather ? (
+          <center>
+            <div id="displayData" className='flex align-center justify-center mt-16 bg-orange-500 w-[300px] h-[450px] p-20 rounded-lg'>
+              <div className='flex h-[100px] w-[100%] items-center justify-center'>
+                <div id='weather' key={weather.id} className='pt-[150px]'>
+                  <h2 className='text-2xl font-italic'>{weather.sys.country}, {weather.name}</h2>
+                  <p className='text-xl font-bold'>{getWeatherEmoji(weather.weather[0].id)} {weather.weather[0].main}</p>
+                  <p className='text-xl font-bold'>{weather.weather[0].description}</p>
+                  <p className='text-xl font-bold'>Temperature: {(weather.main.temp - 273.15).toFixed(2)}°C</p>
+                  <p className='text-xl font-bold'>Humidity: {weather.main.humidity}%</p>
                 </div>
-              ))}
+              </div>
             </div>
-          
-          </div>
+          </center>
         ) : null}
+      </div>
     </>
   );
 }
 
 export default App;
+
